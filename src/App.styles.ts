a1d40e7ed9661e85281e2adea73bfa2c611034ad
Wesly <weslyramalho@yahoo.com.br>
2022-01-26 18:44:08 -0300

    Criando estrutura com styled-components

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Header = styled.div`
+
+`
+export const HeaderText = styled.h1`
+`
+export const Body = styled.div``
